da5acef603a7bdabfadc6765f2b5795ecc1b857b
silverbryan <soporte.g4a@gmail.com>
2021-01-16 15:21:05 -0300

    agreando y configurando el servidor de node

---
+++ server/src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+require("./db.js");
+
+const server = express();
+
+server.name = "API-XCODE";
+
+server.use(morgan("dev"));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(
+    cors({
+        origin: "*",
+        credentials: true,
+    })
+);
+
+server.use("/api/v1/", routes);
+
+// Error catching endware.
+server.use((err, req, res, next) => {
+    // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
+
+module.exports = server;
